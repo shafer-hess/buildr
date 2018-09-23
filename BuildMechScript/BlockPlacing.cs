d4a8f5602b7211ed61f739d2fa558a3f90bf3bbd
GuJialu <35847476+GuJialu@users.noreply.github.com>
2018-09-23 19:36:28 -0400

    build sence script

---
+++ BuildMechScript/BlockPlacing.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockPlacing : MonoBehaviour {
+    public GameObject currentBlock;
+    public Transform blockPreviewPos;
+    public GameObject theBuild;
+
+    public Material warningMaterial;
+    private Material normalMaterial;
+
+    public bool painting;
+    public bool paused = false;
+    public Material paintMaterial;
+
+    // Use this for initialization
+    void Start () {
+        ChooseBlock("Cube0");
+        //theBuild = GameObject.Find("TheShip");
+        normalMaterial = currentBlock.GetComponent<Renderer>().material;        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //stop building when paused
+        if (paused)
+        {
+            return;
+        }
+
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        Debug.DrawRay(ray.origin, ray.direction * 1011, Color.green);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit))
+        {
+
+            //Check if the cube allows palcing on the x and z axis
+            if (hit.normal != Vector3.up && currentBlock.GetComponent<CheckOverlapping>().fixUp)
+            {
+                return;
+            }
+            float distance = Vector3.Dot(currentBlock.GetComponent<BoxCollider>().bounds.size, hit.normal);
+            distance = Mathf.Abs(distance);
+            //Debug.Log(">>>>>size" + currentBlock.GetComponent<BoxCollider>().bounds.size);
+            //Debug.Log(">>>>>distance: " + distance);
+            Vector3 pos = hit.point + distance/2*hit.normal;
+            pos.x = Mathf.Round(pos.x / 2.5f) * 2.5f;
+            pos.y = Mathf.Round(pos.y / 2.5f) * 2.5f;
+            pos.z = Mathf.Round(pos.z / 2.5f) * 2.5f;
+            currentBlock.transform.position = pos;
+
+            //check if placable
+            //Debug.Log("hit" + currentBlock.GetComponent<CheckOverlapping>().overlapped);
+            if (currentBlock.GetComponent<CheckOverlapping>().overlapped)
+            {
+                SetCubeMaterial(currentBlock, warningMaterial);
+                return;
+            }
+            SetCubeMaterial(currentBlock, normalMaterial);
+
+            //placing
+            if (Input.GetMouseButtonDown(0))
+            {
+                
+                GameObject b = Instantiate(currentBlock, theBuild.transform, true);
+                Destroy(b.GetComponent<Rigidbody>());
+                b.layer = LayerMask.NameToLayer("Default");
+            }
+            //deleting
+            if (Input.GetMouseButtonDown(1))
+            {
+                if (theBuild.transform.childCount > 1)
+                {
+                    Destroy(hit.collider.gameObject);
+                }
+                else
+                {
+                    Debug.Log(">>>>cannot delete");
+                }             
+            }
+        }
+        else
+        {
+            if (currentBlock)
+            {
+                currentBlock.transform.position = blockPreviewPos.position;
+            }
+        }
+	}
+
+    public void ChooseBlock (string blockName)
+    {
+        if (currentBlock)
+        {
+            Destroy(currentBlock);
+        }
+        currentBlock = (GameObject)Instantiate(Resources.Load(blockName), blockPreviewPos.position, Quaternion.identity);
+        currentBlock.layer = LayerMask.NameToLayer("Ignore Raycast");
+        Rigidbody rb = currentBlock.AddComponent<Rigidbody>();
+        rb.isKinematic = true;
+        painting = false;
+    }
+
+
+
+    public void SetCubeMaterial(GameObject cube, Material newMaterial)
+    {
+        if (cube.GetComponent<Renderer>())
+        {
+            cube.GetComponent<Renderer>().material = newMaterial;
+        }
+        else
+        {
+            Renderer[] childCubes = cube.GetComponentsInChildren<Renderer>();
+            foreach(Renderer r in childCubes)
+            {
+                if (!r.material.name.Contains("Fixed")&& !r.material.name.Contains("Particle"))
+                {
+                    r.material = newMaterial;
+                }
+            }
+        }
+    }
+}
---
+++ BuildMechScript/ViewControl.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewControl : MonoBehaviour {
+
+    public float speed = 6f;
+
+    Vector3 movement;
+
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2, locked = 3 }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityX = 15F;
+    public float sensitivityY = 15F;
+
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+
+    public float minimumY = -90F;
+    public float maximumY = 90F;
+
+    public Transform target;
+
+    float rotationY = 0F;
+
+    // Use this for initialization
+    void Start () {
+        target = null;
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        float z = Input.GetAxisRaw("updown");
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        
+
+        if (axes == RotationAxes.MouseXAndY)
+        {
+            movement = v * speed * Camera.main.transform.forward + h * speed * Camera.main.transform.right + z * speed * Vector3.up;
+            Camera.main.transform.position += movement;
+            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+        }
+        else if (axes == RotationAxes.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+        }
+        else if (axes == RotationAxes.MouseY)
+        {
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+            transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+        }
+        else
+        {
+            movement = v * speed * Vector3.forward + h * speed * Vector3.right + z * speed * Vector3.up;
+            Camera.main.transform.position += movement;
+        }
+    }
+
+    public void lockRota()
+    {
+        if (axes == RotationAxes.locked)
+        {
+            axes = RotationAxes.MouseXAndY;
+        }
+        else
+        {
+            axes = RotationAxes.locked;
+        }
+    }
+}
