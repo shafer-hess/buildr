1e34241f5d9db2cdcd45ad13d610c91a84846c2d
derek2041 <derek2041@gmail.com>
2018-10-04 20:56:04 -0400

    demo in C#
    updated demo

--- Test1/Driver.cs
+++ Test1/Driver.cs
@@ -9,6 +9,11 @@ namespace Test1 // "package" like in java
             Console.WriteLine("Hello world");
             Console.WriteLine("Press any key to continue");
             Console.ReadKey();
+            Console.WriteLine();
+            Console.Write("Please enter your name: ");
+            Console.WriteLine();
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi " + name + "!");
             
             User grr = new User("Gustavo Rodriguez-Rivera", "grr@purdue.edu", 40);
             Console.WriteLine(grr);
