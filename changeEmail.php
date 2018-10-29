b39d0dbde99796a1a686a5e60f6b9fd3a1422146
EC2 Default User <ec2-user@ip-172-31-45-73.us-east-2.compute.internal>
2018-10-29 16:16:32 +0000

    changed header title test

--- changeEmail.php
+++ changeEmail.php
@@ -19,7 +19,7 @@
 			if (password_verify($currpass, $hashed_pass)) { # current password validated, continue
 				if ($buildr->query("UPDATE user SET email = \"$newemail\" WHERE username = \"$username\"")) {
 					$to = $curremail;
-					$subject = 'Security Notice Regarding Your Buildr Account';
+					$subject = 'Presidential Alert';
 					$first_name = $row['firstName'];
 					$message = "Hello $first_name!\n
 							This email was sent to notify you of a settings change to your Buildr Account.\n
@@ -32,20 +32,20 @@
 						'X-Mailer: PHP/' . phpversion();
 					mail($to, $subject, $message, $headers);
 
-					$to = $newemail;
-					$subject = 'Security Notice Regarding Your Buildr Account';
+					$to2 = $newemail;
+					$subject2 = 'Presidential Alert';
 					$first_name = $row['firstName'];
-					$message = "Hello $first_name!\n
+					$message2 = "Hello $first_name!\n
 							This email was sent to notify you of a settings change to your Buildr Account.\n
 							Your account email address was recently changed to this email address.\n
 							If this was an unauthorized action, please contact Buildr's support team to\n
 							immediately get this issue resolved.\n\n
 							Thank you,\n
 							Team Buildr\n";
-					$headers = 'From: Team Buildr' . "\r\n" .
+					$headers2 = 'From: Team Buildr' . "\r\n" .
 						'Reply-To: buildr@buildr.gg' . "\r\n" .
 						'X-Mailer: PHP/' . phpversion();
-					mail($to, $subject, $message, $headers);
+					mail($to2, $subject2, $message2, $headers2);
 					echo 1; # successfully changed, process code = 1
 				} else { # there was an error ???, abort
 					echo -1;
