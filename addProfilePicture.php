5fee40770697d99f198785d2f92894cdcb9b3d2d
EC2 Default User <ec2-user@ip-172-31-45-73.us-east-2.compute.internal>
2018-11-02 04:52:07 +0000

    first try at profile picture links

---
+++ addProfilePicture.php
@@ -0,0 +1,35 @@
+<?php
+	#				====== USAGE ======
+	#		POST or GET "username", "currpass", "picture" forms 
+	#			to "IPv4 Address"/addProfilePicture.php
+	$username = $_GET["username"];
+	$currpass = $_GET["currpass"];
+	$newpicture = $_GET["picture"];
+
+	$buildr = new mysqli('buildr.cuxgs3tcx7pv.us-east-2.rds.amazonaws.com', 'youseethat', 'X2mJaMNR4oCRYXVFrKt6', 'buildr', '3306');
+	if ($buildr->connect_errno) {
+		printf("Connect failed: %s\n", $buildr->connect_error);
+		exit();
+	}
+	if ($result = $buildr->query("SELECT * FROM user WHERE username = \"$username\"")) {
+		if ($row = $result->fetch_array()) { # username exists, continue
+			$hashed_pass = $row['password'];
+			if (password_verify($currpass, $hashed_pass)) { # current password validated, continue
+				if ($buildr->query("UPDATE user SET picture = \"$newpicture\" WHERE username = \"$username\"")) {
+					echo 1; # successfully changed, process code = 1
+				} else { # there was an error ???, abort
+					echo -1;
+				}
+			} else { # current password does not match, abort
+				echo 0;
+			}
+			$result->close();
+		} else { # username was not found, abort
+			echo 3;
+			$result->close();
+		}
+	} else { # error during query ???, abort
+		echo -1;
+	}
+	$buildr->close();
+?>
---
+++ getProfilePicture.php
@@ -0,0 +1,25 @@
+<?php
+	#				====== USAGE ======
+	#			    POST or GET "username" form 
+	#		      to "IPv4 Address"/getProfilePicture.php
+	$username = $_GET["username"];
+
+	$buildr = new mysqli('buildr.cuxgs3tcx7pv.us-east-2.rds.amazonaws.com', 'youseethat', 'X2mJaMNR4oCRYXVFrKt6', 'buildr', '3306');
+	if ($buildr->connect_errno) {
+		printf("Connect failed: %s\n", $buildr->connect_error);
+		exit();
+	}
+	if ($result = $buildr->query("SELECT * FROM user WHERE username = \"$username\"")) {
+		if ($row = $result->fetch_array()) { # username exists, continue
+			$returnPicture = $row['picture'];
+			echo $returnPicture;
+			$result->close();
+		} else { # username was not found, abort
+			echo 3;
+			$result->close();
+		}
+	} else { # error during query ???, abort
+		echo -1;
+	}
+	$buildr->close();
+?>
