8c8727c0dcfa5b5072f99462c49e6ec4737bff8b
EC2 Default User <ec2-user@ip-172-31-45-73.us-east-2.compute.internal>
2018-11-02 05:06:41 +0000

    added return codes for failed connection

--- addProfilePicture.php
+++ addProfilePicture.php
@@ -9,6 +9,7 @@
 	$buildr = new mysqli('buildr.cuxgs3tcx7pv.us-east-2.rds.amazonaws.com', 'youseethat', 'X2mJaMNR4oCRYXVFrKt6', 'buildr', '3306');
 	if ($buildr->connect_errno) {
 		printf("Connect failed: %s\n", $buildr->connect_error);
+		echo -1;
 		exit();
 	}
 	if ($result = $buildr->query("SELECT * FROM user WHERE username = \"$username\"")) {
