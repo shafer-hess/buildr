ec93c772036c0d337a4ad73d581c6c99fd4dda4f
GuJialu <35847476+GuJialu@users.noreply.github.com>
2018-11-18 22:21:15 -0500

    Reporting

---
+++ ReportBuild.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ReportBuild : MonoBehaviour {
+
+    string url = "http://ec2-18-217-185-132.us-east-2.compute.amazonaws.com/";
+
+    IEnumerator ReportBuildingEnum()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get(url + "reportBuild.php?message=" + "dummyBuildName"))
+        {
+            //www.chunkedTransfer = false;
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                string response = www.downloadHandler.text;
+                Debug.Log(response);
+            }
+        }
+    }
+}
