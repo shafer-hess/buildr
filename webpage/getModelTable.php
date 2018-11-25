46c5313080feacf5ad8f750d9db60e8fe38035cd
Shafer <shafer.hess01@gmail.com>
2018-11-25 17:38:26 -0500

    added model tab and model table, deleteModel script to remove models from table with matching name

---
+++ webpage/getModelTable.php
@@ -0,0 +1,41 @@
+<?php
+
+$buildr = new mysqli('buildr.cuxgs3tcx7pv.us-east-2.rds.amazonaws.com', 'youseethat', 'X2mJaMNR4oCRYXVFrKt6', 'buildr', '3306');
+
+if(file_exists('/var/www/html/reports.json')) { unlink('/var/www/html/reports.json'); }
+
+if ($buildr->connect_errno) {
+    printf("Connection to database failed: %s\n", $buildr->connect_error);
+    exit();
+}
+
+$query_request = "SELECT * FROM models";
+$response = array();
+$models = array();
+
+if ($result = $buildr->query($query_request)) {
+    while($row = $result->fetch_array()) {
+        $buildData = $row['buildData'];
+        $buildName = $row['buildName'];
+        $username = $row['username'];
+        $location = $row['location'];
+        $privacy = $row['privacy'];
+        $avatar = $row['avatar'];
+        $upvotes = $row['upvotes'];
+
+        $models[] = array('Build Data' => $buildData, 'Build Name' => $buildName, 'Username' => $username,
+         'Location' => $location, 'Privacy' => $privacy, 'Avatar' => $avatar, 'Upvotes' => $upvotes);
+    }
+
+    $response['models'] = $models;
+    
+    $file = fopen('/var/www/html/models.json', 'x+');
+    fwrite($file, json_encode($response, JSON_PRETTY_PRINT));
+    fclose($file);
+}
+
+$buildr->close();
+
+header('Location: dashboard.html#model');
+
+?>
