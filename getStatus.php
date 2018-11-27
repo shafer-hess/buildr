75089f7c228fc735edb2642cb223acbcbc20a32a
EC2 Default User <ec2-user@ip-172-31-45-73.us-east-2.compute.internal>
2018-11-27 04:55:54 +0000

    added getStatus and setStatus

---
+++ getStatus.php
@@ -0,0 +1,31 @@
+<?php
+	#				===== USAGE =====
+	#	POST or GET to "IPv4 address"/getChatMessages.php, no forms needed
+$username = $_GET["username"];
+
+$buildr = new mysqli('buildr.cuxgs3tcx7pv.us-east-2.rds.amazonaws.com', 'youseethat', 'X2mJaMNR4oCRYXVFrKt6', 'buildr', '3306');
+
+if ($buildr->connect_errno) {
+	printf("Connect failed: %s\n", $buildr->connect_error);
+	echo -1;
+	exit();
+}
+
+if ($result = $buildr->query("SELECT * FROM user WHERE username = \"$username\"")) {
+	if ($result->num_rows < 1) {
+		echo 0;
+		$result->close();
+		$buildr->close();
+		exit();
+	} else {
+		# printf("Select returned %d rows.\n", $result->num_rows);
+		$status = "";
+		while ($row = $result->fetch_array()) {
+			$status = $row['status'];
+		}
+		echo $status;
+		$result->close();
+	}
+}
+$buildr->close();
+?>
---
+++ setStatus.php
@@ -0,0 +1,30 @@
+<?php
+	#				====== USAGE ======
+	#		POST or GET "username", "currpass", "newpass" forms 
+	#			to "IPv4 Address"/changePassword.php
+	$username = $_GET["username"];
+	$status = $_GET["status"];
+
+	$buildr = new mysqli('buildr.cuxgs3tcx7pv.us-east-2.rds.amazonaws.com', 'youseethat', 'X2mJaMNR4oCRYXVFrKt6', 'buildr', '3306');
+	if ($buildr->connect_errno) {
+		printf("Connect failed: %s\n", $buildr->connect_error);
+		exit();
+	}
+
+	if ($result = $buildr->query("SELECT * FROM user WHERE username = \"$username\"")) {
+		if ($row = $result->fetch_array()) { # username exists, continue
+			if ($buildr->query("UPDATE user SET status = \"$status\" WHERE username = \"$username\"")) {
+				echo 1; # successfully changed, process code = 1
+			} else { # there was an error ???, abort
+				echo -1;
+			}
+			$result->close();
+		} else { # username was not found, abort
+			echo 3;
+			$result->close();
+		}
+	} else { # error during query ???, abort
+		echo -1;
+	}
+	$buildr->close();
+?>
