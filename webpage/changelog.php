e7b26ecff575b1652d420a8e77a0be3e21551212
Shafer <shafer.hess01@gmail.com>
2018-11-28 19:43:59 -0500

    added md.txt file writing in addition to html changelog for easy parse and value setting of SimpleMDE

--- webpage/changelog.php
+++ webpage/changelog.php
@@ -1,11 +1,17 @@
 <?php
 
     $changelog = $_GET['changelog'];
+    $md = $_GET['md'];
 
     if(file_exists('changelog.txt')) {unlink('changelog.txt');}
+    if(file_exists('md.txt')) {unlink('md.txt');}
 
     $file = fopen('changelog.txt', 'x+');
     fwrite($file, $changelog);
+    fclose($file);
+
+    $file = fopen('md.txt', 'x+');
+    fwrite($file, $md);
     fclose($file);
 
 ?>
